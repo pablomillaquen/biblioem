7680e8675f4debdd2aa432da9efa495cf729a37b
Pablo Millaquen <pablomillaquen@gmail.com>
2017-09-14 17:43:21 -0300

    Modificacion routing, falta arreglar formulario ejemplo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing, appRoutingProviders } from './app.routing';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent} from './navbar/navbar.component';
 import { SidebarComponent} from './sidebar/sidebar.component';
 import { BodyappComponent} from './bodyapp/bodyapp.component';
-import { EmpleadoComponent} from './empleado/empleado.component'
+import { EmpleadoComponent} from './empleado/empleado.component';
+
 
 @NgModule({
   declarations: [
@@ -17,9 +21,11 @@ import { EmpleadoComponent} from './empleado/empleado.component'
   ],
   imports: [
     FormsModule,
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,9 +2,15 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 import { EmpleadoComponent} from './empleado/empleado.component';
+import { BodyappComponent} from './bodyapp/bodyapp.component';
 
 const appRoutes: Routes = [
-	{path:'', component:EmpleadoComponent},
+	{path:'', component:BodyappComponent},
+	{path:'bodyapp', component:BodyappComponent},
 	{path:'empleado', component:EmpleadoComponent},
-	{path:'**', component:EmpleadoComponent}
+	{path:'**', component:BodyappComponent}
 ];
+
+export const appRoutingProviders: any[] = [];
+
+export const routing:ModuleWithProviders = RouterModule.forRoot(appRoutes);
